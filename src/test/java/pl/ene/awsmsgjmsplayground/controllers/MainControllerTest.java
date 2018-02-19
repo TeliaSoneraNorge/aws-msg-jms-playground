2a97d835bfd09ce9a1112f07cf394dcb1670991e
Vladimir Jarotek <vladimir.jarotek@telia.no>
2018-02-19 13:18:27 +0100

    test removed

--- src/test/java/pl/ene/awsmsgjmsplayground/controllers/MainControllerTest.java
+++ src/test/java/pl/ene/awsmsgjmsplayground/controllers/MainControllerTest.java
@@ -11,12 +11,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath*:jms-sqs-messaging.xml")
+//@ContextConfiguration("classpath:jms-sqs-messaging.xml")
 public class MainControllerTest {
 
-    @Autowired
+  /*  @Autowired
     private NotificationMessagingTemplate orderMessagingTemplate;
-
+*/
     @Before
     public void setUp() throws Exception {
     }
